e1880faed5bd424f7fcc129b42e8dad8588dad7b
Priya <Priyadarshini.professional@gmail.com>
2018-03-14 10:58:27 +0530

    First commit

---
+++ SendingWhatsupmessage/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WhatsAppApi;
+
+namespace SendingWhatsupmessage
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string from = "7503065558";
+            string to = textBox1.Text;
+            string msg = textBox2.Text;
+            WhatsApp wa = new WhatsApp(from, "adhajfhsdjhsdjg", "priya", false, false);
+            wa.OnConnectSuccess += () =>
+              {
+                  MessageBox.Show("connected to whatsup");
+                  wa.OnLoginSuccess += (phoneNumeber, data) =>
+                  wa.SendMessage(to, msg);
+                  MessageBox.Show("Message sent......");
+
+              };
+            wa.OnLoginFailed += (data) =>
+              {
+                  MessageBox.Show("Login failed...." + data);
+              };
+            wa.Login();
+            
+        }
+
+       
+    }
+}
